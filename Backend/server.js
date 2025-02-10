44d5296e2e675148383d9ce33452a7f68d41e8ac
muhammedarshadtp <arshadtp89@gmail.com>
2025-02-10 10:14:55 +0530

    Fix build script

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors";
-
+import path from 'path'
 import authRoutes from "./Routes/auth.Route.js"
 import messageRoutes from "./Routes/message.Route.js"
 import userRouter from "./Routes/user.Route.js"
@@ -10,7 +10,7 @@ import userRouter from "./Routes/user.Route.js"
 import connectToMongoDB from "./DB/connectToMongoDB.js"
 import { app, server } from "./Socket/socket.js";
 
-
+const __dirname = path.resolve()
 
 const PORT = process.env.PORT || 4000
 
@@ -24,11 +24,13 @@ app.use("/api/auth/", authRoutes)
 app.use("/api/message/", messageRoutes)
 app.use("/api/users/",userRouter)
 
+app.use(express.static(path.join(__dirname,"/Frontend/dist")))
 
-// app.get("/", (req, res) => {
-//     res.send("hello world!")
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"Frontend","dist","index.html"))
 
-// })
+})
+
 
 server.listen(PORT, () => {
     connectToMongoDB();
